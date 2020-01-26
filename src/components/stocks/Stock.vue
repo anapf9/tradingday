296f31b6e615a0b2dca495ded64c888277b972e8
Ana <ana.eng.dev@gmail.com>
2020-01-26 14:54:04 -0300

    create a list of stocks ussing css flexbox propreties

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,7 +1,17 @@
 <template>
-    <div>
-        <h1>Stock</h1>
-    </div>
+    <v-flex class="pr-3 pb-3" xs12 md6 lg4>
+        <v-card class="green darken-3 white--text">
+            <v-card-title class="headline">
+                <strong>
+                    NOME <small> (Preço: PREÇO)</small>
+                </strong>
+            </v-card-title>
+        </v-card>
+        <v-container fill-height>
+            <v-text-field label="Quantidade" type="number"></v-text-field>
+            <v-btn class="green darken-3 white--text">Comprar</v-btn>
+        </v-container>
+    </v-flex>
 </template>
 
 <script>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,12 +1,23 @@
 <template>
-    <div>
-        <h1>Stocks</h1>
-    </div>
+    <v-layout row wrap>
+        <Stock v-for="stock in stocks" :key="stock.id">
+    </v-layout>
 </template>
 
 <script>
-
+import Stock from './Stock'
 export default {
+    components: { Stock },
+    data() {
+        return {
+            stocks: [
+                {id: 1, name:'BMW', price: 100},
+                {id: 1, name:'Google', price: 200},
+                {id: 1, name:'Apple', price: 300},
+                {id: 1, name:'Twitter', price: 50}
+            ]
+        }
+    }
 
 }
 </script>
