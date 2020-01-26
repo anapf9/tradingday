d1d1bbd4fe089d00c430f25148bd2a3c45947436
Ana <ana.eng.dev@gmail.com>
2020-01-26 14:34:59 -0300

    v-menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,30 @@
 <template>
-    <div>
-        <h1>Header</h1>
-    </div>
+  <v-toolbar app>
+    <v-toolbar-title class="headline text-uppercase mr-4">
+    <span>Stock</span>
+    <span class="font-weight-light">Trader</span>
+    </v-toolbar-title>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat to="/">Inicio</v-btn>
+      <v-btn flat to="/portfolio">Portifólio</v-btn>
+      <v-btn flat to="/stocks">Ações</v-btn>
+    </v-toolbar-items>
+    <v-spacer></v-spacer>
+    <v-toolbar-items>
+        <v-btn flat> FInalizar o dia</v-btn>
+        <v-menu offset-y>
+            <v-btn flat slot="activator"> Salvar & Carregar</v-btn>
+            <v-list>
+                <v-list-tile>
+                    <v-list-tile-title> Salvar Dados</v-list-tile-title>
+                </v-list-tile>
+                <v-list-tile>
+                    <v-list-tile-title> Carregar Dados</v-list-tile-title>
+                </v-list-tile>
+            </v-list>
+        </v-menu>
+    </v-toolbar-items>
+  </v-toolbar>
 </template>
 
 <script>
