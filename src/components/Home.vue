1194c273227450f0e467083b1dcfc77fd103a182
Ana <ana.eng.dev@gmail.com>
2020-01-26 18:38:13 -0300

    currency filter for R$ in prices

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,7 +10,7 @@
             <span class="white--text font-weight-light"> Clique em finalizar dia para começar um novo dia</span>
         </v-sheet>
         <v-divider class="my-4" /> 
-        <p class="display-1"><strong>Seu Saldo:</strong> {{funds}} </p>
+        <p class="display-1"><strong>Seu Saldo:</strong> {{funds | currency }} </p>
     </div>
 </template>
 
